6d8a76e970a0df3ef888c56e611155434abcb04f
Chisom Ejim <ejimchisom@gmail.com>
2022-08-06 08:18:49 +0100

    implement open api

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,8 +7,26 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Support\Facades\Artisan;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests, Respondable;
+
+    /**
+     * Display our routes
+     *
+     * @return string
+     */
+    public function routes(): string
+    {
+        Artisan::call('route:list');
+        $routes = explode("\n", Artisan::output());
+        foreach ($routes as $index => $route) {
+            if (str_contains($route, 'debugbar')) {
+                unset($routes[$index]);
+            }
+        }
+        return '<pre>' . implode("\n", $routes) . '</pre>';
+    }
 }
